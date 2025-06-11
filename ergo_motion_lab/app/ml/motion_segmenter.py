8a0c847868c4b7cc957be295cd34c934026081a3
radhityaz <rosita.yuliati2@gmail.com>
2025-06-11 12:24:48 +0700

    Delete ergo_motion_lab directory

--- ergo_motion_lab/app/ml/motion_segmenter.py
+++
@@ -1,7 +0,0 @@
-class MotionSegmenter:
-    """Dummy motion segmenter."""
-
-    def predict(self, activity_window):
-        """Return a placeholder activity label."""
-        # TODO: implement real segmentation
-        return "IDLE"
