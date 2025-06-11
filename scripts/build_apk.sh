1fdd883b4c9563a124d3f7b48577d907be03be5f
radhityaz <rosita.yuliati2@gmail.com>
2025-06-11 17:28:19 +0700

    Update Flutter version and build script

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -4,4 +4,5 @@ set -e
 export PUB_HOSTED_URL="${PUB_HOSTED_URL:-https://pub.flutter-io.cn}"
 export FLUTTER_STORAGE_BASE_URL="${FLUTTER_STORAGE_BASE_URL:-https://storage.flutter-io.cn}"
 
-flet build apk android
+FLUTTER_SDK="$HOME/flutter"
+flet build apk android --flutter-sdk "$FLUTTER_SDK"
