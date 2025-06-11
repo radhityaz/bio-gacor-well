c78703bb3adefbe0feefb13e1fd72e0382f98696
radhityaz <rosita.yuliati2@gmail.com>
2025-06-11 16:37:46 +0700

    Update CI to preinstall Flutter

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# Use existing Flutter SDK if present to avoid redundant downloads
-FLUTTER_VERSION="3.27.4"
-FLUTTER_DIR="$HOME/flutter/$FLUTTER_VERSION"
-if [ -x "$FLUTTER_DIR/bin/flutter" ]; then
-  export PATH="$FLUTTER_DIR/bin:$PATH"
-fi
+export PUB_HOSTED_URL="${PUB_HOSTED_URL:-https://pub.flutter-io.cn}"
+export FLUTTER_STORAGE_BASE_URL="${FLUTTER_STORAGE_BASE_URL:-https://storage.flutter-io.cn}"
 
 flet build apk android
