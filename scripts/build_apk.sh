6f1ca8496c851d5128a75b582c480ef8356aca92
radhityaz <rosita.yuliati2@gmail.com>
2025-06-11 17:28:38 +0700

    Fix Flutter symlink and build command

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -4,5 +4,4 @@ set -e
 export PUB_HOSTED_URL="${PUB_HOSTED_URL:-https://pub.flutter-io.cn}"
 export FLUTTER_STORAGE_BASE_URL="${FLUTTER_STORAGE_BASE_URL:-https://storage.flutter-io.cn}"
 
-FLUTTER_SDK="$HOME/flutter"
-flet build apk android --flutter-sdk "$FLUTTER_SDK"
+flet build apk android
